eb63b3ae06c3a0a55a66a185a6c3e3857b01c543
sam <semuall0108@gmail.com>
2026-02-16 22:44:25 -0800

    finished prototype

--- Program.cs
+++ Program.cs
@@ -1,93 +1,75 @@
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.IdentityModel.Tokens;
-using System.Text;
+    using Microsoft.AspNetCore.Authentication.JwtBearer;
+    using Microsoft.IdentityModel.Tokens;
+    using System.Text;
 
-using pq_chat_httpserver.Realtime;
-using pq_chat_httpserver.Realtime.Options;
-using pq_chat_httpserver.Realtime.Services;
+    using pq_chat_httpserver.Services;
+    using pq_chat_httpserver.Database;
 
-using pq_chat_httpserver.Services;
-using pq_chat_httpserver.Database;
+    var builder = WebApplication.CreateBuilder(args);
 
-var builder = WebApplication.CreateBuilder(args);
+    builder.Services.AddControllers();
+    // Services
+    builder.Services.AddScoped<UserService>();
+    builder.Services.AddScoped<FriendshipService>();
+    builder.Services.AddScoped<ConversationService>();
+    builder.Services.AddScoped<JwtService>();
 
-builder.Services.AddControllers();
-// Services
-builder.Services.AddScoped<UserService>();
-builder.Services.AddScoped<FriendshipService>();
-builder.Services.AddScoped<ConversationService>();
-builder.Services.AddScoped<JwtService>();
+    // Databases
+    builder.Services.AddScoped<UserDatabase>();
+    builder.Services.AddScoped<FriendshipDatabase>();
+    builder.Services.AddScoped<ConversationDatabase>();
 
-// Databases
-builder.Services.AddScoped<UserDatabase>();
-builder.Services.AddScoped<FriendshipDatabase>();
-builder.Services.AddScoped<ConversationDatabase>();
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddSwaggerGen();
 
-// WebSocket/Gateway services
-builder.Services.Configure<TcpOptions>(builder.Configuration.GetSection("RealtimeTcp"));
-builder.Services.AddSingleton<TokenValidator>();
-builder.Services.AddSingleton<GatewayService>();
-
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-// CORS
-builder.Services.AddCors(options =>
-{
-    options.AddPolicy("AllowAll", policy =>
+    // CORS
+    builder.Services.AddCors(options =>
     {
-        policy
-            .AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader();
+        options.AddPolicy("AllowAll", policy =>
+        {
+            policy
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader();
+        });
     });
-});
 
-// Auth
-var jwtKey = builder.Configuration["Jwt:Key"]
-    ?? throw new Exception("Jwt:Key is missing in configuration.");
+    // Auth
+    var jwtKey = builder.Configuration["Jwt:Key"]
+        ?? throw new Exception("Jwt:Key is missing in configuration.");
 
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
+    builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddJwtBearer(options =>
     {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateIssuer = true,
+            ValidateAudience = true,
+            ValidateLifetime = true,
+            ValidateIssuerSigningKey = true,
 
-        ValidIssuer = builder.Configuration["Jwt:Issuer"],
-        ValidAudience = builder.Configuration["Jwt:Audience"],
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
-    };
-});
+            ValidIssuer = builder.Configuration["Jwt:Issuer"],
+            ValidAudience = builder.Configuration["Jwt:Audience"],
+            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(jwtKey))
+        };
+    });
 
-builder.Services.AddAuthorization();
+    builder.Services.AddAuthorization();
 
-var app = builder.Build();
+    var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    if (app.Environment.IsDevelopment())
+    {
+        app.UseSwagger();
+        app.UseSwaggerUI();
+    }
 
-// Middleware order 
-app.UseCors("AllowAll");
-app.UseAuthentication();
-app.UseAuthorization();
+    // Middleware order 
+    app.UseCors("AllowAll");
+    app.UseAuthentication();
+    app.UseAuthorization();
 
-// Enable WebSockets 
-app.UseWebSockets(new WebSocketOptions
-{
-    KeepAliveInterval = TimeSpan.FromSeconds(20)
-});
+    // Map HTTP controllers 
+    app.MapControllers();
 
-// Map HTTP controllers 
-app.MapControllers();
-
-// Map WebSocket gateway endpoint
-RealtimeGatewayEndpoint.Map(app);
-
-app.Run();
+    app.Run();
--- Realtime/Options/TcpOptions.cs
+++
@@ -1,6 +0,0 @@
-namespace pq_chat_httpserver.Realtime.Options;
-public sealed class TcpOptions
-{
-    public string Host { get; set; } = "127.0.0.1";
-    public int Port { get; set; } = 9001;
-}
